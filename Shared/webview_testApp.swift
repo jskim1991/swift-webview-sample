7cc2dcc1749ff78937dae5107baa646d1a11dd79
kingj <tensaijskim1991@gmail.com>
2022-03-04 21:24:38 +0900

    Initial Commit

---
+++ Shared/webview_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  webview_testApp.swift
+//  Shared
+//
+//  Created by Jaesik Kim on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct webview_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
